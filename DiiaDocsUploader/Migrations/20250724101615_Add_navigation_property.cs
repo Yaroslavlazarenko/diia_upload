eb89a36a39dd33e0e41e5d715b25c7c493c57508
Yaroslavlazarenko <yaroslav.lazarenko.odessa@gmail.com>
2025-07-24 13:24:18 +0300

    Add navigation property

---
+++ DiiaDocsUploader/Migrations/20250724101615_Add_navigation_property.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiiaDocsUploader.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_navigation_property : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentTypes_NameDiia",
+                table: "DocumentTypes",
+                column: "NameDiia",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DocumentTypes_NameUa",
+                table: "DocumentTypes",
+                column: "NameUa",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_DocumentTypes_NameDiia",
+                table: "DocumentTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DocumentTypes_NameUa",
+                table: "DocumentTypes");
+        }
+    }
+}
